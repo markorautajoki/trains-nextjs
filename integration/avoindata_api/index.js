d228d024bea0ff1e8d4c78ffdbbdf9fc83a71b65
Marko Rautajoki <marko.rautajoki@futurice.com>
2022-06-15 18:03:49 +0300

    Use native fetch

--- integration/avoindata_api/index.js
+++ integration/avoindata_api/index.js
@@ -1,7 +1,5 @@
-import fetch from 'node-fetch';
-
 export const API_BASE_URL = 'https://rata.digitraffic.fi/api/v1';
 
-export const getLiveTrains = station => fetch(`${API_BASE_URL}/live-trains?station=${station}`).then(res => res.json());
+export const getLiveTrains = (station) => fetch(`${API_BASE_URL}/live-trains?station=${station}`).then((res) => res.json());
 
-export const getStationsMetadata = () => fetch(`${API_BASE_URL}/metadata/stations`).then(res => res.json());
+export const getStationsMetadata = () => fetch(`${API_BASE_URL}/metadata/stations`).then((res) => res.json());
